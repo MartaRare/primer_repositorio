afeb10992a35fff105949eca9c1b6f76fc8ee441
MartaRare <mrmartaroldan@gmail.com>
2013-05-02 10:40:33 +0000

    Mi primer commit

---
+++ hola_mundo.c
@@ -0,0 +1,15 @@
+// Programa: hola_mundo.c
+// Autor: Marta
+// Version: 1.0
+// Fecha: 02/05/2013
+// Hora: 11:34
+// Descripcion: Muestra una cadena de texto por la salida estandar
+
+#include <stdio.h>
+
+int main(void)
+
+{
+ printf("Hola Mundo");
+ return 0;
+}
